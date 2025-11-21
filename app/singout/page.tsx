afeffa1ff0df42b831f59d729755f33f732fc395
oleksiykholod <51864800+oleksiykholod@users.noreply.github.com>
2025-11-21 11:54:19 +0000

    modified:   app/singout/page.tsx

--- app/singout/page.tsx
+++ app/singout/page.tsx
@@ -4,7 +4,7 @@ export default function SignOutPage() {
   return (
     <div>
       <h5>Are you sure you want to sign out?</h5>
-        <button onClick={async () =>{
+        <button className=" bg-amber-400 w-3xl" onClick={async () =>{
             'use server' 
             await signOut();
         }} >Sign out</button>
