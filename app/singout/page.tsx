d173c96ae1176c4d7b7649d9534fe2c0c31165b0
oleksiykholod <51864800+oleksiykholod@users.noreply.github.com>
2025-11-21 11:58:55 +0000

    modified:   app/singout/page.tsx

--- app/singout/page.tsx
+++ app/singout/page.tsx
@@ -1,12 +1,15 @@
 import { signOut } from "@/auth"
+import { useRouter } from "next/navigation";
  
 export default function SignOutPage() {
+    const router = useRouter();
   return (
     <div>
       <h5>Are you sure you want to sign out?</h5>
         <button className=" bg-amber-400 w-3xl" onClick={async () =>{
             'use server' 
             await signOut();
+            router.push('/');
         }} >Sign out</button>
     </div>
   )
