33251fc403018f684ea3a439a1e02bb35ca6e8cb
oleksiykholod <51864800+oleksiykholod@users.noreply.github.com>
2025-11-21 11:52:47 +0000

    modified:   app/layout.tsx 	modified:   app/singout/page.tsx

--- app/singout/page.tsx
+++ app/singout/page.tsx
@@ -4,14 +4,10 @@ export default function SignOutPage() {
   return (
     <div>
       <h5>Are you sure you want to sign out?</h5>
-      <form
-        action={async (formData) => {
-          "use server"
-          await signOut()
-        }}
-      >
-        <button type="submit">Sign out</button>
-      </form>
+        <button onClick={async () =>{
+            'use server' 
+            await signOut();
+        }} >Sign out</button>
     </div>
   )
 }
