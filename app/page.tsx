5ed9f2f5bd3bd45b401556e87a5aa48ba1e606ea
oleksiykholod <51864800+oleksiykholod@users.noreply.github.com>
2026-03-03 21:30:51 +0200

    ss

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,5 @@ import dynamic from "next/dynamic";
 const VizikomMap = dynamic(() => import("./vizikommap"), { ssr: false });
 
 export default function Home() {
-  return ;
+  return <VizikomMap />;
 }
